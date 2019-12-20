a746719851bfe447b29ebe5a02d6d837f18890ee
Kaung Htet Htun <kogyikaunghtet@gmail.com>
2019-12-21 04:00:33 +0630

    Initial commit

---
+++ GPIO Basics/GPIOtest.py
@@ -0,0 +1,35 @@
+import time
+print 'Enter GPIO BCM number:'
+gpio = raw_input()
+print 'Enter number of blinks you want:'
+blinks = raw_input()
+try:
+    f= open ('/sys/class/gpio/unexport','w')
+    f.write(str(gpio))
+    f.close()
+except IOError as e:
+    lol=0
+f= open ('/sys/class/gpio/export','w')
+f.write(str(gpio))
+f.close()
+path = '/sys/class/gpio/gpio' + gpio + '/direction'
+f= open (path,'w')
+f.write('out')
+f.close()
+i=0;
+path = '/sys/class/gpio/gpio' + gpio + '/value'
+while(i<int(blinks)):
+    f= open (path,'w')
+    f.write('1')
+    f.close()
+    time.sleep(0.5)
+    f= open (path,'w')
+    f.write('0')
+    f.close()
+    time.sleep(0.5)
+    i+=1
+f= open ('/sys/class/gpio/unexport','w')
+f.write(str(gpio))
+f.close()
+print 'Good Bye!'
+
