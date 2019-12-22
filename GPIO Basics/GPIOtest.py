bd7a9a5de3e0194e9dfbef9b95918897b06f5cba
Kaung Htet Htun <kogyikaunghtet@gmail.com>
2019-12-23 04:32:09 +0630

    Initial commit

--- GPIO Basics/GPIOtest.py
+++ GPIO Basics/GPIOtest.py
@@ -3,27 +3,27 @@ print 'Enter GPIO BCM number:'
 gpio = raw_input()
 print 'Enter number of blinks you want:'
 blinks = raw_input()
-f= open ('/sys/class/gpio/export','w')
-f.write(str(gpio))
-f.close()
+file= open ('/sys/class/gpio/export','w')
+file.write(str(gpio))
+file.close()
 path = '/sys/class/gpio/gpio' + gpio + '/direction'
-f= open (path,'w')
-f.write('out')
-f.close()
+file= open (path,'w')
+file.write('out')
+file.close()
 i=0;
 path = '/sys/class/gpio/gpio' + gpio + '/value'
 while(i<int(blinks)):
-    f= open (path,'w')
-    f.write('1')
-    f.close()
+    file= open (path,'w')
+    file.write('1')
+    file.close()
     time.sleep(0.5)
-    f= open (path,'w')
-    f.write('0')
-    f.close()
+    file= open (path,'w')
+    file.write('0')
+    file.close()
     time.sleep(0.5)
     i+=1
-f= open ('/sys/class/gpio/unexport','w')
-f.write(str(gpio))
-f.close()
+file= open ('/sys/class/gpio/unexport','w')
+file.write(str(gpio))
+file.close()
 print 'Good Bye!'
 
