a9fb610614b63593cae3adae8d0d5794539e9941
Kaung Htet Htun <kogyikaunghtet@gmail.com>
2020-01-09 01:14:55 +0630

    Initial commit

---
+++ IoT/cpudata.py
@@ -0,0 +1,18 @@
+import httplib
+import urllib
+import time
+API_key = "8ALM3YDAT2N4G16D"
+while 1:
+        cpu = int(open('/sys/class/thermal/thermal_zone0/temp').read())/1$
+        params = urllib.urlencode({'field1': cpu, 'key': API_key })
+        headers = {"Content-typZZe": "application/x-www-form-urlencoded",$
+        conn = httplib.HTTPConnection("api.thingspeak.com:80")
+        try:
+                conn.request("POST", "/update", params, headers)
+                response = conn.getresponse()
+                print cpu
+                print response.status, response.reason
+                data = response.read()
+                conn.close()
+        except KeyboardInterrupt:
+                break
